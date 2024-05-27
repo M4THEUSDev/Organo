5548595e7309866201974eed74ba9b4195b62f67
M4THEUSDev <assisma2002@gmail.com>
2024-05-27 10:32:30 -0300

    componente header criado

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+function Banner (){
+    // JSX
+     return (
+        <header className='banner'>
+        <img src="imagens/banner.png"  alt="Imagem Banner Topo da Página"/>
+        </header>
+     )
+}
+
+
+
+export default Banner;
